a12eda9116a9cb54ab233bfe3b1015b2601ab159
Mike <randomemail@gmail.com>
2015-03-30 13:57:57 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScratchPad::Application.config.secret_key_base = '774383b377ea5a9683ec39b71d6158093b650c6c18bae4a37aa3ef6a2651c6b8dbb49ec7141c92c1aec92e40636013518c8fdd24f3abc1cd3e933648b5476670'
